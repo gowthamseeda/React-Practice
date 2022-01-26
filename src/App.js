fae6a4c8c5b50117cda46e20707ab4f704bbfc3a
Haripriya Sidda <91542261+haripriya9620@users.noreply.github.com>
2022-01-26 17:49:08 +0530

    PriceCard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import PriceCard from './components/PriceCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let data = [
+    {
+      product:"FREE",
+      price:0,
+      users:"Single User",
+      usersFlag:true,
+      usersHighlight:false,
+      storage:"5GB Storage",
+      storageFlag:true,
+      access:"Community Access",
+      accessFlag:true,
+      support:"Dedicated Phone Support",
+      supportFlag:false,
+      reports:"Monthly Status Report",
+      reportsFlag:false,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsFlag:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsFlag:false,
+      subdomain:"Free Subdomain",
+      subdomainFlag:false,
+      subdomainHighlight:false
+    },
+    {
+      product:"PLUS",
+      price:9,
+      users:"5 Users",
+      usersFlag:true,
+      usersHighlight:true,
+      storage:"50GB Storage",
+      storageFlag:true,
+      access:"Community Access",
+      accessFlag:true,
+      support:"Dedicated Phone Support",
+      supportFlag:true,
+      reports:"Monthly Status Report",
+      reportsFlag:false,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsFlag:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsFlag:true,
+      subdomain:"Free Subdomain",
+      subdomainFlag:true,
+      subdomainHighlight:false
+    },
+    {
+      product:"PRO",
+      price:49,
+      users:"Unlimited Users",
+      usersFlag:true,
+      usersHighlight:true,
+      storage:"150GB Storage",
+      storageFlag:true,
+      access:"Community Access",
+      accessFlag:true,
+      support:"Dedicated Phone Support",
+      supportFlag:true,
+      reports:"Monthly Status Report",
+      reportsFlag:true,
+      publicProjects:"Unlimited Public Projects",
+      publicProjectsFlag:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsFlag:true,
+      subdomain:"Unlimited Free Subdomains",
+      subdomainFlag:true,
+      subdomainHighlight:true
+    }    
+  ]
+  return <>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {
+            data.map((e)=>{
+              return <PriceCard data={e}/>
+          })
+        }
+        </div>
+      </div>
+    </section>
+  </>
 }
 
 export default App;
